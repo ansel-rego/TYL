299dab3ee200ee78290629a9b1a71d33151a4dc9
Ansel Vivian Rego <167329177+ansel-rego@users.noreply.github.com>
2025-03-03 15:00:10 +0530

    Update 3.py

--- 7.String Based/3.py
+++ 7.String Based/3.py
@@ -10,6 +10,7 @@ Sample Output:
 2,3
 '''
 
+#Code starts here:
 str1=input()
 str2=input()
 likes=0
@@ -20,3 +21,4 @@ for i in range(len(str1)):
     elif str1[i]=='0' and str2[i]=='0':
         dislikes+=1
 print(likes,dislikes,sep=',')
+#Code ends here.
