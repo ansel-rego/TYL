b7f2f261b98fefbd63ed5bc3ebe9b2ef4819a328
Ansel Vivian Rego <167329177+ansel-rego@users.noreply.github.com>
2025-03-03 15:04:30 +0530

    Update 7.py

--- 8.String Based/7.py
+++ 8.String Based/7.py
@@ -8,6 +8,7 @@ Sample Output:
 listen silent linest
 '''
 
+#Code starts here:
 str1=input()
 list1=str1.split()
 list2=[]
@@ -18,3 +19,4 @@ for i in range((len(list1))-1):
         if list1[i+1] not in list2:
             list2.append(list1[i+1])
 print(*list2)
+#Code ends here.
