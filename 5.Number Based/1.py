fd3934b36a62dacb2764c4ab2c51ef6c90321eb4
Ansel Vivian Rego <167329177+ansel-rego@users.noreply.github.com>
2025-03-03 14:51:41 +0530

    Update 1.py

--- 5.Number Based/1.py
+++ 5.Number Based/1.py
@@ -7,9 +7,11 @@ Sample Output:
 
 '''
 
+#Code starts here:
 n=int(input())
 list1=[]
 for i in range(1,n+1):
     if (n%i==0) and (i%2!=0):
         list1.append(i)
 print(*list1,sep=' ')
+#Code ends here.
